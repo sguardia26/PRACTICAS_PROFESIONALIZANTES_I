5377eed184e1689c8c49c25a9aba7d53b0b2707e
Tec. Guardia Sebastian Nicolas <sguardia@cen.edu.ar>
2026-08-14 18:33:44 -0300

    main

---
+++ HTML/javascript/script.js
@@ -0,0 +1,9 @@
+//Tipos de Datos y Variables
+//Variable Asignacion
+let entero = 10;
+let decimal = 10.5;
+let caracter = "Practicas";
+let logico = true;
+
+//Mostrar
+console.log(entero+decimal+caracter+logico);
