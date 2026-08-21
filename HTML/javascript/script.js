10c5edcc7ea952bd03d59d5d345482bbeb1de81b
sguardia25 <sguardia@cen.edu.ar>
2026-08-21 19:09:24 -0300

    main

--- HTML/javascript/script.js
+++ HTML/javascript/script.js
@@ -15,4 +15,28 @@ let base = 2;
 let exponente = 3;
 let potencia = base**exponente;
 console.log("La potencia es igual a= ", potencia);
-//RAIZ 
+//RAIZ
+let raiz = 9;
+let resultado = Math.sqrt(raiz);
+console.log("La raiz cuadrada es: ", resultado);
+
+//ESTRUCTURAS DE CONTROL
+//SIMPLE
+//MAYOR DE DOS NUMERO
+let numero1 = 30;
+let numero2 = 20;
+if (numero2 > numero1) {
+    console.log("El mayor es: ", numero2);
+} 
+if (numero1 > numero2) {
+    console.log("El mayor es: ", numero1);
+}
+//DOBLE 
+//NUMERO POSITIVO O NEGATIVO
+let numero3=-20;
+if (numero3 > 0) {
+    console.log("El numero positivo: ",numero3);
+} else {
+    console.log("El numero es negativo: ", numero3);
+}
+
