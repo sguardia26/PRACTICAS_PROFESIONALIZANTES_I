2a32ceb6ad7af099977b71885c0183e945678470
sguardia25 <sguardia@cen.edu.ar>
2026-08-21 20:30:29 -0300

    main

---
+++ HTML/javascript/estructuras.js
@@ -0,0 +1,22 @@
+//CONDICIONALES/SELECTIVAS
+//SIMPLE
+//COMPARAR EL MAYOR DE DOS NUMEROS
+let numero1 = 20;
+let numero2 = 10;
+//PRIMERO MAYOR
+if (numero1 > numero2) {
+    console.log("El mayor es: ", numero1)
+}
+if (numero2 > numero1) {
+    console.log("El mayor es: ", numero2);
+}
+//DOBLE
+//POSITIVO / NEGATIVO / NEUTRO 
+let numero3 = 0;
+if (numero3 > 0) {
+    console.log("El numero es positivo");
+} else if (numero3 < 0 ){
+    console.log("El numero es negativo");
+} else {
+    console.log("El numero es neutro");
+}
