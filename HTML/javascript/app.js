cb9b45444ced332121a0a3c511b6d7949a320ce8
Tec. Guardia Sebastian Nicolas <sguardia@cen.edu.ar>
2026-08-24 17:03:28 -0300

    main

---
+++ HTML/javascript/app.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+//SUMA
+let numero1 = prompt("Ingresar el numero1: ");
+let nuemro2 = prompt("Ingresar el numero2: ");
+
+let suma = nuemro2 + numero1;
+
+console.log("La suma es: ", suma);
