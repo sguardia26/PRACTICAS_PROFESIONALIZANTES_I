6f787f37dba8dbb59fadf20f4a0fee6392bb6ce3
Tec. Guardia Sebastian Nicolas <sguardia@cen.edu.ar>
2026-08-25 17:45:28 -0300

    main

--- HTML/javascript/app.js
+++ HTML/javascript/app.js
@@ -1,10 +1,2 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
-
-//SUMA
-let numero1 = parseInt(prompt("Ingresar el numero1: "));
-let nuemro2 = parseInt(prompt("Ingresar el numero2: "));
-
-let suma = nuemro2 + numero1;
-
-console.log("La suma es: ", suma);
